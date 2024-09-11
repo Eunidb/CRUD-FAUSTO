d11d15cb673d87456bf378ada7dc841fc71a65a8
Eunidb <a3521110133@alumno.uttehuacan.edu.mx>
2024-09-11 14:14:44 -0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./shares/componets/layout/layout.component')
+      .then(m => m.LayoutComponent), // Especifica el componente
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./inicio/dashboard/dashboard.component')
+          .then(m => m.DashboardComponent) // Especifica el componente
+      },
+      {
+        path: 'profile',
+        loadComponent: () => import('./inicio/profile/profile.component')
+          .then(m => m.ProfileComponent) // Especifica el componente
+      },
+      {
+        path: 'tables',
+        loadComponent: () => import('./inicio/table/table.component')
+          .then(m => m.TableComponent) // Especifica el componente
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
